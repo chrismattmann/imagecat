f13752720cf250bcc86d20854d2a6046119eb29d
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 13:49:31 -0700

    Create StopAll.sh

---
+++ StopAll.sh
@@ -0,0 +1,12 @@
+export OODT_HOME= 'pwd'
+echo -- Stopping All Scripts --
+cd $OODT_HOME/bin
+./oodt stop
+cd $OODT_HOME/tomcat7/bin
+./shutdown.sh
+cd $OODT_HOME/resmgr/bin
+./start-memex-stubs
+exit
+killall java
+ps -aef java
+echo [DONE]
