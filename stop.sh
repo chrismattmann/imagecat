879aad1e96a6817d7e0c275f1d3531f5e58bd946
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 16:49:26 -0700

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,12 +1,8 @@
-export OODT_HOME=`pwd`
 echo -- Stopping All Scripts --
 cd $OODT_HOME/bin
 ./oodt stop
 cd $OODT_HOME/tomcat7/bin
 ./shutdown.sh
-cd $OODT_HOME/resmgr/bin
-./start-memex-stubs
-exit
 killall java
-ps -aef java
+ps -aef | grep java
 echo [DONE]
