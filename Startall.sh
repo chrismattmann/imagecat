1baeb131fdf0a09bd1538d5e77089e3316ff78fe
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 13:48:32 -0700

    Create Startall.sh

---
+++ Startall.sh
@@ -0,0 +1,12 @@
+export OODT_HOME= 'pwd'
+echo -- Starting All Required Scripts --
+/bin/bash/
+source bin/imagecatenv.sh
+cd $OODT_HOME/bin
+./oodt start
+cd $OODT_HOME/tomcat7/bin
+./startup.sh
+cd $OODT_HOME/resmgr/bin
+./start-memex-stubs
+exit
+echo [DONE]
