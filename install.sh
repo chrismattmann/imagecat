a43ad5e2f981fbc3bd59d81b191aaa0c23db40d5
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 15:32:57 -0700

    Update and rename Booyah.sh to install.sh

--- install.sh
+++ install.sh
@@ -17,8 +17,8 @@ cp -R ../imagecat/solr4 ./solr4 && cp -R ../imagecat/tomcat7 ./tomcat7
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? tomcat7/conf/Catalina/localhost/solr.xml
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/env.sh
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/imagecatenv.sh
-/bin/bash && source bin/imagecatenv.sh
-exit
+/bin/bash 
+source bin/imagecatenv.sh
 cp filemgr/lib/cas-filemgr-* resmgr/lib
 cp workflow/lib/cas-workflow-* resmgr/lib
 cp crawler/lib/cas-crawler-* resmgr/lib
