726d53d9fedacfa42c4eda4cd00f597128e71d21
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 12:38:54 -0700

    Update imagecatenv.sh

--- distribution/src/main/resources/bin/imagecatenv.sh
+++ distribution/src/main/resources/bin/imagecatenv.sh
@@ -1,4 +1,4 @@
-export OODT_HOME=a[OODT_HOME]a
+export OODT_HOME=--OODT_HOME--
 export FILEMGR_URL="http://localhost:9000"
 export WORKFLOW_URL="http://localhost:9001"
 export RESMGR_URL="http://localhost:9002"
