356cd71898c9484d9816acddeaeabaeba1c41cf6
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-17 15:02:11 -0700

    Update Booyah.sh

--- Booyah.sh
+++ Booyah.sh
@@ -13,11 +13,11 @@ echo [SUCCESS]
 echo - Copying Files and Changing Paths -
 cp -R distribution/target/*.tar.gz ../deploy
 cd ../deploy && tar xvzf *.tar.gz
+cp -R ../imagecat/solr4 ./solr4 && cp -R ../imagecat/tomcat7 ./tomcat7
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? tomcat7/conf/Catalina/localhost/solr.xml
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/env.sh
 sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/imagecatenv.sh
 /bin/bash && source bin/imagecatenv.sh
-cp -R ../imagecat/solr4 ./solr4 && cp -R ../imagecat/tomcat7 ./tomcat7
 cp filemgr/lib/cas-filemgr-* resmgr/lib
 cp workflow/lib/cas-workflow-* resmgr/lib
 cp crawler/lib/cas-crawler-* resmgr/lib
