d18c04ab20ab66327bb4d16be8b48a698450258e
ATZimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-18 11:30:34 -0700

    Update start.sh

--- Auto/start.sh
+++ Auto/start.sh
@@ -2,10 +2,13 @@ export OODT_HOME=`pwd`
 echo -- Starting All Required Scripts --
 source bin/imagecatenv.sh
 cd $OODT_HOME/bin
+echo *******Starting OODT*******
 ./oodt start
 cd $OODT_HOME/tomcat7/bin
+echo *******Starting Tomcat*******
 ./startup.sh
 cd $OODT_HOME/resmgr/bin
+echo ******Starting Memex Stubs******
 ./start-memex-stubs
 exit
 echo [DONE]
