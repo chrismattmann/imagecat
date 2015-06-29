54888b54638e312ba383d5113205076d0be51f35
Andrew Zimdars <andrew.t.zimdars@jpl.nasa.gov>
2015-06-29 11:57:55 -0700

    Update install.sh

--- auto/install.sh
+++ auto/install.sh
@@ -30,9 +30,9 @@ echo - Copying Files and Changing Paths -
 cp -R distribution/target/*.tar.gz ../deploy
 cd ../deploy && tar xvzf *.tar.gz
 cp -R ../imagecat/solr4 ./solr4 && cp -R ../imagecat/tomcat7 ./tomcat7
-sed -i "" s?--OODT_HOME--?${OODT_HOME}? tomcat7/conf/Catalina/localhost/solr.xml
-sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/env.sh
-sed -i "" s?--OODT_HOME--?${OODT_HOME}? bin/imagecatenv.sh
+sed -i  s?--OODT_HOME--?${OODT_HOME}? tomcat7/conf/Catalina/localhost/solr.xml
+sed -i  s?--OODT_HOME--?${OODT_HOME}? bin/env.sh
+sed -i  s?--OODT_HOME--?${OODT_HOME}? bin/imagecatenv.sh
 source bin/imagecatenv.sh
 cp filemgr/lib/cas-filemgr-* resmgr/lib
 cp workflow/lib/cas-workflow-* resmgr/lib
