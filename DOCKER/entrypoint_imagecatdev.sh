11f8f67929c37bd69bbe69c7500c2dcc03e74b4c
Dan LaManna <dan.lamanna@kitware.com>
2015-12-08 17:27:46 -0500

    Add optional SimpleHTTPServer to docker run

--- DOCKER/entrypoint_imagecatdev.sh
+++ DOCKER/entrypoint_imagecatdev.sh
@@ -8,5 +8,11 @@ cd $OODT_HOME/resmgr/bin/ && ./start-memex-stubs
 echo "Docker Container ID:" $HOSTNAME
 pushd $OODT_HOME/logs/
 python -m SimpleHTTPServer &
+
+if [ -n "$IMAGECAT_IMAGE_PATH" ] && [ -d "$IMAGECAT_IMAGE_PATH" ]; then
+    pushd $IMAGECAT_IMAGE_PATH
+    python -m SimpleHTTPServer 9241 &
+fi
+
 echo "Watching /deploy/data/staging/roxy-image-list-jpg-nonzero.txt"
 while inotifywait -e close_write /deploy/data/staging/roxy-image-list-jpg-nonzero.txt; do $OODT_HOME/bin/chunker; done
